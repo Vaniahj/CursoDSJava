38885882b895a5f43a0239683ffd6dd3c1258eb5
Vaniahj <vaniahj@hotmail.com>
2020-08-28 20:48:37 -0300

    Exemplo de tela com botão sair.

---
+++ ExemploBotaoSair/src/exemplobotaosair/ExemploBotaoSair.java
@@ -0,0 +1,37 @@
+package exemplobotaosair;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+/**
+ *
+ * @author Vania Julia
+ */
+public class ExemploBotaoSair extends JFrame{
+    JButton botaosair;
+    
+    public ExemploBotaoSair(){
+        super("Exemplo com JButton");
+        Container tela = getContentPane();
+        setLayout(null);
+        
+        botaosair = new JButton ("Sair");
+        botaosair.setBounds(100,50,100,20);
+        botaosair.addActionListener(
+                new ActionListener(){
+                    public void actionPerformed(ActionEvent e){
+                        System.exit(0);
+                    }
+                }); 
+        tela.add(botaosair);
+        setSize(300,150);
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        ExemploBotaoSair app = new ExemploBotaoSair();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+    
+}
